3529c28e7d3c9ac4b49f5281cc9406d1be607542
ClaudioMendes <97466807+ClaudioMendes@users.noreply.github.com>
2022-02-24 14:29:03 -0300

    Banco_Comercio

--- 03_inserts.sql
+++ 03_inserts.sql
@@ -7,12 +7,12 @@ insert into Cliente (Nome, TipoPessoa,DataCriacao)  values ('Ana', 'J',getdate()
 insert into Cliente (Nome, TipoPessoa,DataCriacao)  values ('Higor', 'F',getdate());
 insert into Cliente (Nome, TipoPessoa,DataCriacao) values  ('Julio','J',getdate());
 
-insert into StatusPedidoItem (Descricao) values ('Separação Solicitada')
-insert into StatusPedidoItem (Descricao) values ('Em Separação')
+insert into StatusPedidoItem (Descricao) values ('SeparaÃ§Ã£o Solicitada')
+insert into StatusPedidoItem (Descricao) values ('Em SeparaÃ§Ã£o')
 insert into StatusPedidoItem (Descricao) values ('Embalado')
 
 insert into Produtos (Nome, Descricao, Preco) values ('Caneta', 'Caneta azul', 1.5)
-insert into Produtos (Nome, Descricao, Preco) values ('Caderno', 'Caderno 10 matérias', 20.99)
+insert into Produtos (Nome, Descricao, Preco) values ('Caderno', 'Caderno 10 matÃ©rias', 20.99)
 
 insert into Pedido (DataSolicitacao, FlagPago, TotalPedido, CodigoCliente, CodigoStatus) values (getdate(),0,3,7,1)
 insert into Pedido (DataSolicitacao, FlagPago, TotalPedido, CodigoCliente, CodigoStatus) values (getdate(),0,22.49,1,1)
@@ -20,8 +20,9 @@ insert into Pedido (DataSolicitacao, FlagPago, TotalPedido, CodigoCliente, Codig
 
 
 insert into PedidoItem (CodigoPedido, CodigoProduto, Preco, Quantidade) values (1, 1, 1.50, 1)
-insert into PedidoItem (CodigoPedido, CodigoProduto, Preco, Quantidade) values (2, 2, 1.50, 2)
-insert into PedidoItem (CodigoPedido, CodigoProduto, Preco, Quantidade) values (3, 3, 20.99, 3)
+insert into PedidoItem (CodigoPedido, CodigoProduto, Preco, Quantidade) values (2, 1, 1.50, 2)
+insert into PedidoItem (CodigoPedido, CodigoProduto, Preco, Quantidade) values (3, 1, 20.99, 3)
+
 
 insert PedidoItemLog (CodigoPedido, CodigoProduto, CodigoStatusPedidoItem, DataMovimento)
 select CodigoPedido, CodigoProduto, 1, getdate()
