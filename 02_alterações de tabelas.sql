875bc51599b64929389c6e2fa59dcdf60be179ea
ClaudioMendes <97466807+ClaudioMendes@users.noreply.github.com>
2022-02-24 14:28:12 -0300

    Banco_Comercio

--- 02_alterações de tabelas.sql
+++ 02_alterações de tabelas.sql
@@ -2,7 +2,7 @@ alter table Cliente add DataCriacao datetime null
 
 alter table Pedido add CodigoStatus int
 
-alter table Cliente add constraint fk_Cliente_pedido foreign key (CodigoPedido) references Pedido (Codigo)
+alter table PedidoItem add constraint fk_produto foreign key (CodigoProduto) references Produtos (Codigo)
 
 alter table PedidoItem add constraint fk_pedido foreign key (CodigoPedido) references Pedido (Codigo)
 
@@ -11,7 +11,3 @@ alter table Pedido add constraint fk_pedido_cliente foreign key (CodigoCliente) 
 alter table PedidoItemLog add constraint fk_PedidoItemLog_PedidoItem foreign key(CodigoPedido, CodigoProduto) references PedidoItem (CodigoPedido, CodigoProduto)
 
 alter table PedidoItemLog add constraint fk_PedidoItemLog_StatusPedidoItem foreign key (CodigoStatusPedidoItem) references StatusPedidoItem (Codigo)
-
-
-
-
